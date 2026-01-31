b82d945179cd277acc442172a2f0d5fed3afae51
Sneha Singh <157780499+asdfghjklasssdf@users.noreply.github.com>
2026-01-31 09:24:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,13 @@ import notificationRoutes from "./routes/notificationRoutes.js";
 
 dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,          // allow Render + localhost
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
